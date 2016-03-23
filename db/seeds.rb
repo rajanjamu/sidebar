f8807393abebd6f47f258d823ece2ecdbfeddadb
Rajan Gupta <rajanjamu@gmail.com>
2016-03-23 15:19:36 +0530

    Added seed employee data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Employee.create(name: "Harish");
+Employee.create(name: "Rakesh");
+Employee.create(name: "Manoj");
+Employee.create(name: "Deepak");
